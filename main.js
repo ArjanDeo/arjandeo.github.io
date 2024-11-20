02ce490dbf8644c83e28e403b2ca6909e08e9f19
Arjan Deo <42519389+ArjanDeo@users.noreply.github.com>
2024-11-20 20:48:29 +0000

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-function checkGayness() {
-    var inputName = document.getElementById('inputName').value.toLowerCase();
-    if (inputName === "tony deo" || inputName === "anuvinder deo") {
-        var div = document.getElementById('identityDiv');
-        div.style.display = "block";
-    } else {
-        alert("Not Gay");
-    }
-}
